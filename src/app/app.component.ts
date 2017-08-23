4921cb79a95929346dbc01f58ca45e73fdde6dd9
japps1c07 <japps1c07@cicct.local>
2017-08-23 10:00:36 +0800

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,63 +4,61 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
-
 })
 export class AppComponent {
   studentCollection: Array<object> = [];
   studentRecord: object;
-
   studNo: number;
   studFname: string;
   studLname: string;
   studProg: string;
   studYr: number;
 
-  message = '';
-  printing  = false;
+  messages = '';
+  printing = false;
 
-  private checkPatterns(value: any, pattern: RegExp): boolean {
+  private checkPatterns(value:any , pattern: RegExp): boolean {
     if (pattern.test(value))
-      return true;
-    else return false;
-
+    return true;
+    else
+      return false;
   }
 
-addStudentEntry(): Boolean {
-  this.printing = false;
-  const stringPattern = /^[A-z\s]+$/;
+addStudentEntry(): Boolean{
+  this.printing=false;
+  const stringPattern =  /^[A-z\s]+$/;
   const studNumberPattern = /^[0-9]+$/;
   const studYearPattern = /^[1-5]+$/;
 
-  if (this.checkPatterns(this.studNo, studNumberPattern) && this.checkPatterns(this.studFname, stringPattern) &&
-  this.checkPatterns(this.studLname, stringPattern) && this.checkPatterns(this.studYr, studYearPattern) && 
-  this.checkPatterns(this.studProg, stringPattern)){
+  if(this.checkPatterns(this.studNo, studNumberPattern) &&
+  this.checkPatterns(this.studFname, stringPattern)&&
+  this.checkPatterns(this.studLname, stringPattern)&&
+  this.checkPatterns(this.studProg, stringPattern)&&
+  this.checkPatterns(this.studYr, studYearPattern)){
+    this.studentRecord = {
+      studNumber: this.studNo,
+      studFirstName: this.studFname,
+      studLastName: this.studLname,
+      sutdProgram: this.studProg,
+      studYear: this.studYr
+    };
+    this.studentCollection.push(this.studentRecord);
+    this.messages = null;
+    this.clearValues();
+  }else{
+    this.messages = 'Errors have been encountered and therefore cannot continue';
+    return false;
+  
 
-  this.studentRecord = {
-    studNumber: this.studNo,
-    studFirstName: this.studFname,
-    studLastName: this. studLname,
-    studProgam: this.studProg,
-    studYear: this.studYr
   }
-
-  this.studentCollection.push(this.studentRecord);
-  this.message = null;
-  this.clearValues();
-  }
-
-else {
-  this.message = 'Error';
-  return false;
+  
 }
-}
-
-listStudents(): void {
+listStudents(): void{
   this.printing = true;
-  console.log('Showing Stored Students');
+  console.log('Showing stored students');
 }
 
-clearValues(): void {
+clearValues(): void{
   this.studNo = null;
   this.studFname = null;
   this.studLname = null;
@@ -69,5 +67,3 @@ clearValues(): void {
 }
 
 }
-
-
