cf92f0b7f3c21ffa15ebf90fc8ddf2d08c013739
japps1c07 <japps1c07@cicct.local>
2017-08-23 09:48:54 +0800

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,70 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+
 })
 export class AppComponent {
-  title = 'app';
+  studentCollection: Array<object> = [];
+  studentRecord: object;
+
+  studNo: number;
+  studFname: string;
+  studLname: string;
+  studProg: string;
+  studYr: number;
+
+  message = '';
+  printing  = false;
+
+  private checkPatterns(value: any, pattern: RegExp): boolean {
+    if (pattern.test(value))
+      return true;
+    else return false;
+
+  }
+
+addStudentEntry(): Boolean {
+  this.printing = false;
+  const stringPattern = /^[A-z\s]+$/;
+  const studNumberPattern = /^[0-9]+$/;
+  const studYearPattern = /^[1-5]+$/;
+
+  if (this.checkPatterns(this.studNo, studNumberPattern) && this.checkPatterns(this.studFname, stringPattern) &&
+  this.checkPatterns(this.studLname, stringPattern) && this.checkPatterns(this.studYr, studYearPattern) && 
+  this.checkPatterns(this.studProg, stringPattern)){
+
+  this.studentRecord = {
+    studNumber: this.studNo,
+    studFirstName: this.studFname,
+    studLastName: this. studLname,
+    studProgam: this.studProg,
+    studYear: this.studYr
+  }
+
+  this.studentCollection.push(this.studentRecord);
+  this.message = null;
+  this.clearValues();
+  }
+
+else {
+  this.message = 'Error';
+  return false;
 }
+}
+
+listStudents(): void {
+  this.printing = true;
+  console.log('Showing Stored Students');
+}
+
+clearValues(): void {
+  this.studNo = null;
+  this.studFname = null;
+  this.studLname = null;
+  this.studProg = null;
+  this.studYr = null;
+}
+
+}
+
+
